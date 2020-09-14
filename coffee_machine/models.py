03ea6d1560fb977743e6976a2657d948592b931a
marina-fares <marina.fares11@gmail.com>
2020-09-15 00:32:15 +0200

    update models

--- coffee_machine/models.py
+++ coffee_machine/models.py
@@ -1,19 +1,35 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
-product_type_mach = (
-    ('COFFEE_MACHINE_LARGE','COFFEE_MACHINE_LARGE'),
-    ('COFFEE_MACHINE_SMALL','COFFEE_MACHINE_SMALL'),
-    ('ESPRESSO_MACHINE','ESPRESSO_MACHINE')
-)
-
-model_type = (
-    ('base_model','base_model'),
-    ('premium_model','premium_model'),
-    ('deluxe_model','deluxe_model'),
-)
 class Machine(models.Model):
+
+    COFFEE_MACHINE_LARGE = 'CM10'
+    COFFEE_MACHINE_SMALL = 'CM00'
+    ESPRESSO_MACHINE = 'EM00'
+
+    product_type_mach = (
+        (COFFEE_MACHINE_LARGE,'COFFEE_MACHINE_LARGE'),
+        (COFFEE_MACHINE_SMALL,'COFFEE_MACHINE_SMALL'),
+        (ESPRESSO_MACHINE,'ESPRESSO_MACHINE')
+    )
+
+    model_type = (
+        ('1','base_model'),
+        ('2','premium_model'),
+        ('3','deluxe_model'),
+    )
     product_type_machine = models.CharField( max_length=50, choices= product_type_mach)
     model_types = models.CharField( max_length=50, choices= model_type)
     water_line_compatible = models.BooleanField()
+    sku = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.sku = ''.join((slugify(self.product_type_machine), slugify(self.model_types)))
+        super(Machine, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.sku
+    
--- coffee_pods/models.py
+++ coffee_pods/models.py
@@ -1,30 +1,40 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 product_type_pod = (
-    ('COFFEE_POD_LARGE','COFFEE_POD_LARGE'),
-    ('COFFEE_POD_SMALL','COFFEE_POD_SMALL'),
-    ('ESPRESSO_POD','ESPRESSO_POD')
+    ('CP1','COFFEE_POD_LARGE'),
+    ('CP0','COFFEE_POD_SMALL'),
+    ('EP0','ESPRESSO_POD')
 )
 
 coffee_flavor = (
-    ('COFFEE_FLAVOR_VANILLA','COFFEE_FLAVOR_VANILLA'),
-    ('COFFEE_FLAVOR_CARAMEL','COFFEE_FLAVOR_CARAMEL'),
-    ('COFFEE_FLAVOR_PSL','COFFEE_FLAVOR_PSL'),
-    ('COFFEE_FLAVOR_MOCHA','COFFEE_FLAVOR_MOCHA'),
-    ('COFFEE_FLAVOR_HAZELNUT','COFFEE_FLAVOR_HAZELNUT'),
+    ('0','COFFEE_FLAVOR_VANILLA'),
+    ('1','COFFEE_FLAVOR_CARAMEL'),
+    ('2','COFFEE_FLAVOR_PSL'),
+    ('3','COFFEE_FLAVOR_MOCHA'),
+    ('4','COFFEE_FLAVOR_HAZELNUT'),
 )
 
 pack_size = (
-    ('1_dozen','1_dozen'),
-    ('3_dozen','3_dozen'),
-    ('5_dozen','5_dozen'),
-    ('7_dozen','7_dozen'),
+    ('1','1_dozen'),
+    ('3','3_dozen'),
+    ('5','5_dozen'),
+    ('7','7_dozen'),
 )
 
 class Pods(models.Model):
     product_type_pods = models.CharField( max_length=50, choices= product_type_pod)
     coffee_flavors = models.CharField( max_length=50, choices= coffee_flavor)
     pack_sizes = models.CharField( max_length=50, choices= pack_size)
+    sku = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.sku = ''.join((slugify(self.product_type_pods), slugify(self.coffee_flavors), slugify(self.pack_sizes)))
+        super(Pods, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.sku
     
